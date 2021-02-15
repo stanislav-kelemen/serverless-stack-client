0bea3d5008e7db61bd92a08d5c147146a0fe836b
Stanislav Kelemen <kelemen.work@gmail.com>
2021-02-15 19:35:16 +0200

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import ListGroup from "react-bootstrap/ListGroup";
 import { BsPencilSquare } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
@@ -43,6 +44,14 @@ function renderLander() {
     <div className="lander">
       <h1>Scratch</h1>
       <p className="text-muted">A simple note taking app</p>
+      <div className="pt-3">
+        <Link to="/login" className="btn btn-info btn-lg mr-3">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
+      </div>
     </div>
   );
 }
